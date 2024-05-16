2201a67f80244545ccb61ff20237b232828cb9da
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-16 10:05:44 -0500

    lambda aws para despliegue

--- functions/server.js
+++ functions/server.js
@@ -3,17 +3,19 @@ import chrome from 'chrome-aws-lambda';
 import express from 'express';
 import serverless from 'serverless-http';
 import cors from 'cors';
+import os from 'os';
 
 const app = express();
 
-// // Habilitar CORS
-// app.use(cors());
+// Habilitar CORS
+app.use(cors());
 
 const router = express.Router();
 
 async function getDataZIM(number, type, sealine) {
     const url = `https://www.searates.com/es/container/tracking/?number=${number}&type=${type}&sealine=${sealine}`;
 
+    // Configuración del path para Chrome en Netlify
     const executablePath = await chrome.executablePath;
 
     try {
@@ -70,3 +72,39 @@ if (process.env.NODE_ENV !== 'production') {
     app.use('/', router); // Configurar el router para que funcione localmente
     app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 }
+
+// Script adicional para automatizar tareas
+(async() => {
+    // Configuración del path para Chrome en Netlify
+    const executablePath = await chrome.executablePath;
+
+    const browser = await puppeteer.launch({
+        executablePath,
+        args: chrome.args,
+        headless: chrome.headless,
+    });
+    const page = await browser.newPage();
+
+    // Navegar a la URL
+    await page.goto('https://developer.chrome.com/');
+
+    // Configurar el tamaño de la pantalla
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Escribir en el cuadro de búsqueda
+    await page.type('.devsite-search-field', 'automate beyond recorder');
+
+    // Esperar y hacer clic en el primer resultado
+    const searchResultSelector = '.devsite-result-item-link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    // Localizar el título completo con una cadena única
+    const textSelector = await page.waitForSelector('text/Customize and automate');
+    const fullTitle = textSelector ? await textSelector.evaluate(el => el.textContent) : 'No title found';
+
+    // Imprimir el título completo
+    console.log('The title of this blog post is "%s".', fullTitle);
+
+    await browser.close();
+})();
