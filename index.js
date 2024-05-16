ebf8836693a12aa8b06ff6b5a4ff7cf1e81d1c4a
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-15 21:08:18 -0500

    Cambio Api - Datos visualización en json v1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 import express from 'express';
 import fs from 'fs';
 
@@ -7,29 +7,27 @@ const app = express();
 async function getDataZIM(number, type, sealine) {
     const url = `https://www.searates.com/es/container/tracking/?number=${number}&type=${type}&sealine=${sealine}`;
 
-    const browser = await puppeteer.launch({
-        slowMo: 200,
-        headless: true,
-    });
-    const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+        const page = await browser.newPage();
 
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(url);
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(url, { waitUntil: 'networkidle2' });
 
-    const element1 = await page.evaluateHandle(() => {
-        return document.querySelector("#tracking_system_root").shadowRoot.querySelector("#app-root > div.jNVSgr > div.sTC0fR > div.OZ_R4c");
-    });
+        const element1 = await page.evaluateHandle(() => {
+            return document.querySelector("#tracking_system_root").shadowRoot.querySelector("#app-root > div.jNVSgr > div.sTC0fR > div.OZ_R4c");
+        });
 
-    const html1 = await page.evaluate(element => element.outerHTML, element1);
+        const html1 = await page.evaluate(element => element.outerHTML, element1);
 
-    const element2 = await page.evaluateHandle(() => {
-        return document.querySelector("#tracking_system_root").shadowRoot.querySelector("#app-root > div.jNVSgr > div.bQFM_E");
-    });
-
-    await element2.screenshot({ path: 'data/element2.png' });
-    await browser.close();
-
-    return { html1 };
+        await browser.close();
+        return { html1 };
+    } catch (error) {
+        console.error("Error occurred while getting data from ZIM:", error);
+        throw error;
+    }
 }
 
 app.get('/getDataZIM', async(req, res) => {
@@ -43,10 +41,10 @@ app.get('/getDataZIM', async(req, res) => {
         const htmls = await getDataZIM(number, type, sealine);
         res.json(htmls);
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error en la ruta /getDataZIM:', error);
         res.status(500).send('Error interno del servidor');
     }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
