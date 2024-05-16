e6f2e49350f047850f1be6dc978fc1c3d9e9dc89
LeonardoHuelvas <lejhubo01@hotmail.com>
2024-05-15 19:24:58 -0500

    Creación Api y actualización

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 import fs from 'fs';
-// import pdf from 'html-pdf';
+import pdf from 'html-pdf';
 import express from 'express';
 
 const app = express();
@@ -14,45 +14,55 @@ async function getDataZIM(number, type, sealine) {
     });
     const page = await browser.newPage();
 
-    // Set the viewport to 1920x1080
+    // Establecer el tamaño de la ventana a 1920x1080
     await page.setViewport({ width: 1920, height: 1080 });
 
     await page.goto(url);
 
-    // Extracting the first element using a CSS selector
+    // Extraer el primer elemento usando un selector CSS
     const element1 = await page.evaluateHandle(() => {
         return document.querySelector("#tracking_system_root").shadowRoot.querySelector("#app-root > div.jNVSgr > div.sTC0fR > div.OZ_R4c");
     });
 
-    // Capture the first element in a PNG
+    // Capturar el primer elemento en un PNG
     await element1.screenshot({ path: 'data/element1.png' });
 
-    // Save the HTML of the first element
+    // Guardar el HTML del primer elemento
     const html1 = await page.evaluate(element => element.outerHTML, element1);
     fs.writeFileSync('data/element1.html', html1);
 
-    // Convert the HTML to PDF
+    // Convertir el HTML a PDF
     pdf.create(html1).toFile('data/element1.pdf', function(err, res) {
         if (err) return console.log(err);
         console.log(res);
     });
 
-    // Extracting the second element using a CSS selector
+    // Extraer el segundo elemento usando un selector CSS
     const element2 = await page.evaluateHandle(() => {
         return document.querySelector("#tracking_system_root").shadowRoot.querySelector("#app-root > div.jNVSgr > div.bQFM_E");
     });
 
-    // Capture the second element in a PNG
+    // Capturar el segundo elemento en un PNG
     await element2.screenshot({ path: 'data/element2.png' });
 
-    // Closing the browser
+    // Cerrar el navegador
     await browser.close();
 }
 
-app.get('/getDataZIM', (req, res) => {
+app.get('/getDataZIM', async(req, res) => {
     const { number, type, sealine } = req.query;
-    getDataZIM(number, type, sealine);
-    res.send('Datos capturados y guardados.');
+
+    if (!number || !type || !sealine) {
+        return res.status(400).send('Faltan parámetros de consulta requeridos: number, type, sealine');
+    }
+
+    try {
+        await getDataZIM(number, type, sealine);
+        res.send('Datos capturados y guardados.');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error interno del servidor');
+    }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
